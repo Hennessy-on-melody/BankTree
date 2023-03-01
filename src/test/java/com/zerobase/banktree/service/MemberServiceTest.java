a0398bce1d87d1d73fde78d734407232ed22f7cf
Hennessy-on-melody <attempt3699@gmail.com>
2023-03-01 16:32:15 +0900

    Member 엔터티 수정/ MemberRepository save, find 로직 생성

--- src/test/java/com/zerobase/banktree/service/MemberServiceTest.java
+++ src/test/java/com/zerobase/banktree/service/MemberServiceTest.java
@@ -2,13 +2,16 @@ package com.zerobase.banktree.service;
 
 import com.zerobase.banktree.domain.member.Member;
 import com.zerobase.banktree.repository.MemberRepository;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @Transactional
 class MemberServiceTest {
     @Autowired
@@ -26,6 +29,7 @@ class MemberServiceTest {
         Long saveId = memberService.joinService(member);
 
         //then
+        Assert.assertSame(member.getLoginId(), "aaa");
 
     }
 }
